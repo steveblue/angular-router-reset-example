aad3bd1da6d1d48f62569d9b36dfb89d70210dc4
Steve Belovarich <steveblue@gmail.com>
2017-09-23 09:45:02 -0700

    Revert @angular to 4.2.2 to test production build

--- main.prod.js
+++ main.prod.js
@@ -3,3 +3,4 @@ import { enableProdMode } from '@angular/core';
 import { AppModuleNgFactory } from './ngfactory/src/app/app.module.ngfactory';
 enableProdMode();
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
+//# sourceMappingURL=main.prod.js.map
